9bbf63f216b86f057fbe8dd65a7809c85f91a461
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2024-09-05 11:25:42 +0100

    Initial commit

---
+++ SortedRecipes/SortedRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortedRecipesApp.swift
+//  SortedRecipes
+//
+//  Created by Vanessa Hurla on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SortedRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
