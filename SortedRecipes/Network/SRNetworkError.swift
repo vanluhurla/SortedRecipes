9ffdb6576327023a29fe8d5fe33b65ecd1d778b0
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2024-09-05 15:58:10 +0100

    Create Network, URL, Error, Model and UnitTest files.

---
+++ SortedRecipes/Network/SRNetworkError.swift
@@ -0,0 +1,15 @@
+//
+//  SRNetworkError.swift
+//  SortedRecipes
+//
+//  Created by Vanessa Hurla on 05/09/2024.
+//
+
+import Foundation
+
+enum SRNetworkError: Error {
+    case invalidURL
+    case unknown
+    case noData
+    case decodingError
+}
---
+++ SortedRecipes/Network/SRNetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  SRNetworkManager.swift
+//  SortedRecipes
+//
+//  Created by Vanessa Hurla on 05/09/2024.
+//
+
+import Foundation
+
+class GCNetworkManager {
+    
+    private var urlSession: URLSession
+    private var urlString: String
+    
+    init(urlSession: URLSession = .shared, urlString: String) {
+        self.urlSession = urlSession
+        self.urlString = urlString
+    }
+    
+    func getRecipeList(completion: @escaping (_ recipes: [SRRecipe]?, _ error: SRNetworkError?) -> Void) {
+        guard let url = URL(string: SRNetworkUrl.recipeList) else {
+            completion(nil, SRNetworkError.invalidURL)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data,
+                  error == nil else {
+                completion(nil, SRNetworkError.noData)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .formatted(.serverDateFormat)
+                let recipeRecord = try decoder.decode(SRRecipeRecord.self, from: data)
+                completion(recipeRecord.record.recipes, nil)
+            } catch {
+                completion(nil, SRNetworkError.decodingError)
+            }
+        }
+        task.resume()
+    }
+}
